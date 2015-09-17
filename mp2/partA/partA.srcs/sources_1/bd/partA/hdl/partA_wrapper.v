02bfa4c77c076d79f62f9c40bd8745f31d2af5fd
Tan H. Nguyen <thnguyn2@illinois.edu>
2015-09-16 22:48:10 -0500

    Add part A project

---
+++ mp2/partA/partA.srcs/sources_1/bd/partA/hdl/partA_wrapper.v
@@ -0,0 +1,24 @@
+//Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2015.1 (lin64) Build 1215546 Mon Apr 27 19:07:21 MDT 2015
+//Date        : Wed Sep 16 22:35:25 2015
+//Host        : zombie running 64-bit Ubuntu 14.04.3 LTS
+//Command     : generate_target partA_wrapper.bd
+//Design      : partA_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module partA_wrapper
+   (gpio2_io_i,
+    gpio_io_i);
+  input [31:0]gpio2_io_i;
+  input [31:0]gpio_io_i;
+
+  wire [31:0]gpio2_io_i;
+  wire [31:0]gpio_io_i;
+
+  partA partA_i
+       (.gpio2_io_i(gpio2_io_i),
+        .gpio_io_i(gpio_io_i));
+endmodule
---
+++ mp2/partA/partA.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2015.1 (lin64) Build 1215546 Mon Apr 27 19:07:21 MDT 2015
+-- Date        : Wed Sep 16 22:46:20 2015
+-- Host        : zombie running 64-bit Ubuntu 14.04.3 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/thnguyn2/source_code/ECE_527_MP/mp2/partA/partA.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.vhdl
+-- Design      : blk_mem_gen_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity blk_mem_gen_0 is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addra : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end blk_mem_gen_0;
+
+architecture stub of blk_mem_gen_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,wea[0:0],addra[3:0],dina[31:0],douta[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_2,Vivado 2015.1";
+begin
+end;
