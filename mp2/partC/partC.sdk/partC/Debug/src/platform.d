d1e75cb50950aa9c1ed2415eccbf7e33afcf7647
Tan H. Nguyen <thnguyn2@illinois.edu>
2015-09-23 23:24:17 -0500

    Created project for part C and finished PS code for summing

---
+++ mp2/partC/partC.sdk/partC/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.d: ../src/platform.c \
+ ../../partC_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../partC_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../partC_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../partC_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../partC_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../partC_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../partC_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../partC_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
